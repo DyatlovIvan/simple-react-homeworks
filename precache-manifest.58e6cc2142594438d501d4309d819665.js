fc2fdc2596eb80debaac42590ef32b4a0fa92d2e
DyatlovIvan <diatlovivan92@mail.ru>
2021-10-17 17:39:43 +0400

    Updates

---
+++ precache-manifest.58e6cc2142594438d501d4309d819665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66becae497ad57580ce50b407f720bf8",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "a16d2315e06b85d9f26f",
+    "url": "/simple-react-homeworks/static/css/main.d2d57302.chunk.css"
+  },
+  {
+    "revision": "e2b7511f9e4869391f71",
+    "url": "/simple-react-homeworks/static/js/2.a662048b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-homeworks/static/js/2.a662048b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16d2315e06b85d9f26f",
+    "url": "/simple-react-homeworks/static/js/main.2370cf5a.chunk.js"
+  },
+  {
+    "revision": "af4ec8013c5fe87b14da",
+    "url": "/simple-react-homeworks/static/js/runtime-main.f59815ab.js"
+  }
+]);
