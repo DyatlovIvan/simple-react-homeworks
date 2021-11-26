80d39a565d849e2016739670d8a8effcca84f070
DyatlovIvan <diatlovivan92@mail.ru>
2021-11-26 14:51:07 +0400

    hw11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 import s from '../h11/common/c8-SuperDoubleRange/SuperDoubleRange.module.css'
-import SuperDoubleRange2 from "./common/c9-SuperDoubleRange/SuperDoubleRange2";
 
 function HW11() {
     const [value1, setValue1] = useState(0)
@@ -35,7 +34,6 @@ function HW11() {
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
-            <SuperDoubleRange2 min={1} max={2}/>
             <hr/>
         </div>
     )
